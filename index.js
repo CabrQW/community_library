8c48d48eba5c5440f5291417265c47069589e040
CabrQW <carlosbom64@gmail.com>
2025-08-31 22:53:22 -0300

    feat:finally

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 import express from 'express'
-import useRouters from './src/routes/user.routes.js'
-import bookRouters from './src/routes/book.routes.js'
-import loanRouters from './src/routes/loan.routes.js'
+import { routers }from './src/routes/index.js'
 import "./src/services/cron.service.js"
 import "dotenv/config"
 const app = express()
@@ -9,9 +7,8 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
-app.use(useRouters)
-app.use(bookRouters)
-app.use(loanRouters)
+app.use(routers)
+
 
 
 
