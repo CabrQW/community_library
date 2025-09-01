7fca1b22a1d05cfe021cc6457227a7bad9125eaa
CabrQW <carlosbom64@gmail.com>
2025-08-31 21:41:05 -0300

    feat: CRUD de empréstimo

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 import express from 'express'
 import useRouters from './src/routes/user.routes.js'
 import bookRouters from './src/routes/book.routes.js'
+import loanRouters from './src/routes/loan.routes.js'
+import "dotenv/config"
 const app = express()
 
 const port = process.env.PORT || 3000
+
 app.use(express.json())
 app.use(useRouters)
 app.use(bookRouters)
+app.use(loanRouters)
 
 
 
